204157f11bb21bc383b7e86e867ccf6fdf6e2b6f
geekboysupreme <shuvam.manna@gmail.com>
2018-10-02 19:25:50 +0530

    update and photo grid tests

---
+++ assets/assets-custom/index.js
@@ -0,0 +1,14 @@
+function getRandomSize(min, max) {
+  return Math.round(Math.random() * (max - min) + min);
+}
+
+var allImages = "";
+for (var i = 0; i < 105; i++) {
+
+    var width = getRandomSize(200, 400);
+    var height =  getRandomSize(200, 400);
+    allImages += '<img src="https://placekitten.com/'+width+'/'+height+'" alt="pretty kitty">';
+
+}
+
+$('#photos').append(allImages);
